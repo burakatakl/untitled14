751ca75045f3d129e21d273761da4a9989ff9315
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-15 20:58:30 +0100

    hayvan adi degisti

--- hayvan.h
+++ hayvan.h
@@ -3,27 +3,30 @@
 #ifndef UNTITLED14_HAYVAN_H
 #define UNTITLED14_HAYVAN_H
 
-#endif //UNTITLED14_HAYVAN_H
-
-class Hayvan{
+class Hayvan {
 public:
-   Hayvan* SetName(std::string _name){
-       adi = _name;
-       return this;
-   }
-    Hayvan* SetAnimal(std::string _type){
+    Hayvan *SetName(std::string _name) {
+        adi = _name;
+        return this;
+    }
+
+    Hayvan *SetAnimal(std::string _type) {
         turu = _type;
         return this;
     }
-    Hayvan* SetKind(std::string _kind){
-       cinsi = _kind;
-       return this;
+
+    Hayvan *SetKind(std::string _kind) {
+        cinsi = _kind;
+        return this;
     }
-    Hayvan* SetAge(int _age){
+
+    Hayvan *SetAge(int _age) {
         yas = _age;
         return this;
     }
+
     void Print();
+
 private:
     std::string adi;
     std::string turu;
@@ -31,3 +34,5 @@ private:
     int yas;
 
 };
+
+#endif //UNTITLED14_HAYVAN_H
