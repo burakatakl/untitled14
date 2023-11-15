8788ccc98d98a8a8c4209cf0ad24e79a0d95a74c
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-15 21:00:01 +0100

    mis

--- hayvan.h
+++ hayvan.h
@@ -9,20 +9,33 @@ public:
         adi = _name;
         return this;
     }
+    std::string& GetName(){
+        return adi;
+    }
 
     Hayvan *SetAnimal(std::string _type) {
         turu = _type;
         return this;
     }
+    std::string& GetAnimal(){
+        return turu;
+    }
+
 
     Hayvan *SetKind(std::string _kind) {
         cinsi = _kind;
         return this;
     }
+    std::string& GetKind(){
+        return cinsi;
+    }
 
     Hayvan *SetAge(int _age) {
         yas = _age;
         return this;
+    }
+    int& GetAge(){
+        return yas;
     }
 
     void Print();
